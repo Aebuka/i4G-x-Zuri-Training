0373dcd10c187c95e3195bb367326dde56882c7a
Aebuka <48730736+Aebuka@users.noreply.github.com>
2022-05-28 15:46:37 +0100

    Finding anagrams task

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,22 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+from collections import Counter
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+        if (Counter(word) == Counter(anagram)):
+            print("True")
+        # elif(Counter(word) != Counter(anagram)):
+        #         print("False")
+        else: print("False")
+        return
+
+
+inp1 = (input("Enter your first word: "))
+inp2 = (input("Enter your first word: "))
+
+find_anagram(inp1,inp2)
